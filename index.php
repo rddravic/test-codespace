4154b7eeac73f633e0ca2b70657b4f323f5d4b98
Ravichand RDD <rddravic@users.noreply.github.com>
2022-11-29 16:32:32 +0000

    test client file

--- index.php
+++ index.php
@@ -1,38 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Bootstrap 5 Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-</head>
+<html>
 <body>
+<script>
+function validateForm() {
+  let x = document.getElementById("fname").value;
+  if (x == "") {
+    alert("Name must be filled out");
+  } else {
+  	document.getElementById("res").innerHTML = x;
+    // Consume the function as follows:
+    const file = createBlob(x);
+    saveAs(file, "myFile.txt");
+  }
+}
 
-<div class="container-fluid p-5 bg-primary text-white text-center">
-  <h1>My First Bootstrap Page</h1>
-  <p>Resize this responsive page to see the effect!</p> 
-</div>
-  
-<div class="container mt-5">
-  <div class="row">
-    <div class="col-sm-4">
-      <h3>Column 1</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
-    <div class="col-sm-4">
-      <h3>Column 2</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
-    <div class="col-sm-4">
-      <h3>Column 3</h3>        
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
-  </div>
-</div>
+function createBlob(data) {
+  return new Blob([data], { type: "text/plain" });
+}
+
+function saveAs() {
+  const a = document.createElement("a");
+  a.href = './myFile.txt';
+  a.download = "myFile.txt";
+  a.click();
+}
+</script>
+
+<div>
+  <textarea id="fname"></textarea><br>
+  <button onClick="validateForm()"> Submit </button>
+</div> 
+
+<p id="res"></p>
 
 </body>
 </html>
+
